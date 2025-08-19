367605c92da74c5b7a3d28793cadf3983e86de6b
Laboratório 04 <lab04@acadunirn.lan>
2025-08-19 20:46:22 -0300

    CRUD finalizando

--- Banco_de_Dados/db/commands.sql
+++ Banco_de_Dados/db/commands.sql
@@ -18,4 +18,14 @@ INSERT INTO cursos (id, nome, aulas) VALUES (1, 'Banco de Dados', 15);
 
 INSERT INTO cursos (id, nome, aulas) VALUES (2, 'JavaScript', 5);
 
+DELETE FROM "cursos" WHERE "id" = 1;
+
+SELECT id, nome FROM cursos;
+
+SELECT * FROM cursos LIMIT 1;
+
+SELECT * FROM "cursos" WHERE "id" = 1;
+
+UPDATE cursos SET nome = 'HTML e CSS', aulas = 20 WHERE id = 1;
+
 
