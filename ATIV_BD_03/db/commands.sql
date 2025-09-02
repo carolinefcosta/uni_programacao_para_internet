63faf86a17ea8a7734d1f5670c284965fcd9469a
Laboratório 04 <lab04@acadunirn.lan>
2025-09-02 20:22:19 -0300

    Atividade 03 ajustes

--- ATIV_BD_03/db/commands.sql
+++ ATIV_BD_03/db/commands.sql
@@ -15,5 +15,24 @@ CREATE TABLE compras (
     id INTEGER NOT NULL,
     cliente_id INTEGER NOT NULL,
     produto_id INTEGER NOT NULL,
-    data NOT NULL
+    data TEXT NOT NULL
+
 );
+
+DROP TABLE clientes;
+DROP TABLE compras;
+DROP TABLE produtos;
+
+INSERT INTO produtos (id, nome, preco) 
+    VALUES (1, 'Notebook', 1000),
+        (2, 'Smartphone', 500), 
+        (3, 'Tablet', 300);
+
+INSERT INTO clientes (id, nome, email) 
+    VALUES (1, 'Maria', 'maria@email.com'),
+        (2, 'João', 'joao@email.com');
+
+INSERT INTO compras (id, client_id, produto_id, data) 
+    VALUES (1, 2, 1, '2049-01-01'),
+        (2, 1, 2, '2049-01-02'),
+        (1, 2, 3, '2049-01-03');
